0f07cd71c22403888737786ce183fae10e4c2e96
pedro silva <pedrro2@hotmail.com>
2016-07-03 17:52:35 -0300

    adding consumer-contract test

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 #SPRING-BOOT CONSUMING REST API
 [Article - Tutorial](https://spring.io/guides/gs/consuming-rest/)
+
+##Consumer Contract Testing
+Also on this repository has a POC of consumer contract test.
+
+###How to run
+
+* npm install
+* npm run contract
---
+++ src/test/resources/contract-test/contract-test.js
@@ -0,0 +1,21 @@
+var Contract = require('consumer-contracts').Contract;
+var Joi = require('consumer-contracts').Joi;
+
+module.exports = new Contract({
+    name: 'gturnquist-quoters',
+    consumer: 'test',
+    request: {
+        method: 'GET',
+        url: 'http://gturnquist-quoters.cfapps.io/api/random'
+    },
+    response: {
+        statusCode: 200,
+        body: Joi.object().keys({
+                                    type: Joi.string(),
+                                    value: Joi.valueOf({
+                                                                 id: Joi.number().integer(),
+                                                                 quote: Joi.string()
+                                                             })
+                                })
+    }
+});
